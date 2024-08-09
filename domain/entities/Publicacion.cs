20d9c69775a9fe1f2cd67dfd2de710c0423faccd
Jeremy Castro <140206171+Shermy143@users.noreply.github.com>
2024-08-08 20:18:07 -0500

    Add files via upload

---
+++ domain/entities/Publicacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain.entities
+{
+    public class Publicacion
+    {
+        public int Id { get; set; }
+        public string Imagen { get; set; }
+        public string Titulo { get; set; }
+        public float Precio { get; set; }
+        public string Categoria { get; set; }
+        public bool Estado { get; set; }
+        public string Detalles { get; set; }
+    }
+}
