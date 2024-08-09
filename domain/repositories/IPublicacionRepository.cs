b35a36d61a940a89f261371a6112dbe73a4f8944
Jeremy Castro <140206171+Shermy143@users.noreply.github.com>
2024-08-08 20:18:33 -0500

    Add files via upload

---
+++ domain/repositories/IPublicacionRepository.cs
@@ -0,0 +1,19 @@
+﻿using domain.entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain.repositories
+{
+    public interface IPublicacionRepository
+    {
+        Task<int> Crear(Publicacion publicacion);
+        Task<int> CambiarEstadoActivo(int id);
+        Task<Publicacion?> ObtenerPorId(int id);
+        Task<List<Publicacion>> ObtenerTodos();
+        Task EliminarPublicacion(int id);
+        Task<int> ModificarPublicacion(Publicacion publicacion);
+    }
+}
